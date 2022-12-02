f9a9941eccc8d4816f5f7986248b5d530ce32501
odai307 <gabrielafotey@gmailcom>
2022-12-02 14:15:05 +0000

    print names defined

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
+    import sys
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
