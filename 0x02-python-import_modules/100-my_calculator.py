fc23a09625ecd19f07b00ccad451cf1d9fdb4f4b
odai307 <gabrielafotey@gmailcom>
2022-12-02 12:38:36 +0000

    fix it

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1 as cal
+    from calculator_1 import add, sub, mul, div
     import sys
 
-    count = len(sys.argv) -1
-    operations = ["+", "-", "/", "*"]
+    count = len(sys.argv)
+    operators = ["+", "-", "/", "*"]
+    argv = sys.argv[1:]
     
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     else:
-        if "+" in sys.argv:
-            print("Usage: {} {} + {} = {}".format(sys.argv[0], sys.argv[1], sys.argv[2], int(sys.argv[1]) + int(sys.argv[2])))
-        elif "-" in sys.argv:
-            print("Usage: {} {} - {} = {}".format(sys.argv[0], sys.argv[1], sys.argv[2], int(sys.argv[1]) - int(sys.argv[2])))
-        elif "/" in sys.argv:
-            print("Usage: {} {} / {} = {}".format(sys.argv[0], sys.argv[1], sys.argv[2], int(sys.argv[1]) / int(sys.argv[2])))
-        elif "*" in sys.argv:
-            print("Usage: {} {} * {} = {}".format(sys.argv[0], sys.argv[1], sys.argv[2], int(sys.argv[1]) * int(sys.argv[2])))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-
-
-
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} / {:d} = {:d}".format(a, b, mul(a, b)))
 
