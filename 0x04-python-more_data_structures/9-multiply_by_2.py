5f7305ffa8eea44d6e1eb38e1b3487f70d204df6
odai307 <gabrielafotey@gmail.com>
2022-12-11 00:10:35 +0000

    update dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     temp = dict(a_dictionary)
     for key, value in temp.items():
         temp[key] = value * 2
-    return temp
+        print("{}: {:d}".format(key, value))
