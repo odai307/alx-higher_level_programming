8e46a6b73b5e64334b0ec5ff9f4b39bbe0a07ffc
odai307 <gabriel>
2022-12-21 23:01:25 +0000

    printing the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# 4-square.py
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a class square"""
+    def __init__(self, size=0):
+        """
+        Initializes the class
+        Args:
+            if size is an integer raise a TypeError
+            if size is less than 0, raise a ValueError
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        @property
+        def size(self):
+            """Retrieves size of square"""
+            return self.__size
+
+        @property.setter
+        def size(self, value):
+            """Set the size to value"""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+        def area(self):
+            """Returns the area of the square"""
+            return self.__size ** 2
+
+        def my_print(self):
+            """prints # the number of times the value of size"""
+            if self.__size == 0:
+                print()
+            for i in range(self.__size):
+                print("#" * self.__size)
