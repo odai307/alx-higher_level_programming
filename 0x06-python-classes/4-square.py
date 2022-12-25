505189aee89141a653e371f1b15c43db06ea3df6
odai307 <gabriel>
2022-12-25 23:39:00 +0000

    update ve value

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,10 +16,6 @@ class Square:
             ValueError:
                 if size is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
@@ -34,7 +30,7 @@ class Square:
         If value is less than 0, raise a ValueError"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
