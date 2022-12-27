2b6331a9ab2a6801ab0a5ba37064184516214f98
odai307 <gabrielafotey@gmail.com>
2022-12-27 22:39:57 +0000

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -9,6 +9,11 @@ class Square:
         """
         Initializes the class
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
 
         @property
