2efd75853eeee1d0b96db0899ebe180d0a625097
odai307 <gabrielafotey@gmail.com>
2022-12-27 22:33:01 +0000

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -23,6 +23,7 @@ class Square:
                 raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")
+            self.__size = value
 
         def area(self):
             """Returns the area of the square"""
