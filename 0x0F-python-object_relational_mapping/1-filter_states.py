93c3bc8f8d8182e288584d9f05e6be4c18382181
Gabriel Afotey <gabrielafotey@gmail.com>
2023-03-23 02:10:49 +0000

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""Module to list all states starting with upper case n (N)
+    from the hbtn_0e_0_usa database"""
 
 import MySQLdb
 from sys import argv
 
-"""This is a script that lists all states with a name starting with
-    Uppercase N in the database hbtn_0e_0_usa"""
-if __name__ == "__main__":
+if __name__ == "__nain__":
     db = MySQLdb.connect(
-            host="localhost", port=3306, name=argv[1],
+            host="localhost", port=3306, user=argv[1],
             passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
+    cursor = db.cursor
     cursor.execute("SELECT * FROM states \
             WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cursor.fetchall()
